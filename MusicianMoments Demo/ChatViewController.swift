376bef445d24de25a3ac9965cb88697207d5fb9f
Mishali Pandey <mishali.pandey@gmail.com>
2023-11-04 11:11:29 -0700

    finsihed search

--- MusicianMoments Demo/ChatViewController.swift
+++ MusicianMoments Demo/ChatViewController.swift
@@ -88,7 +88,8 @@ class ChatViewController: MessagesViewController {
         view.backgroundColor = .red
 
        
-        messages.append(Message(sender: currentSender, messageId: "10293", sentDate: Date().addingTimeInterval(-26400), kind: .text("Sped IN The head")))
+        messages.append(Message(sender: currentSender, messageId: "1", sentDate: Date().addingTimeInterval(-26400), kind: .text("Sped IN The head")))
+  
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
@@ -122,8 +123,7 @@ extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, Messag
         return Sender(photoURL: "", senderId: "", displayName: "")
     }
     
-
-
+    
 
 
     }
--- MusicianMoments Demo/SearchViewController.swift
+++ MusicianMoments Demo/SearchViewController.swift
@@ -7,23 +7,86 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UISearchBarDelegate {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var field: UISearchBar!
+    
+    var data = [String]()
+    var filteredData = [String]()
+    var filtered = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "YourCellIdentifier")
+        
+        setupData()
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text {
+            filterText(text + string)
+            return true
+        }
+        return false
     }
-    */
-
+    
+    func filterText(_ query: String?) {
+        filteredData.removeAll()
+        for string in data {
+            if string.lowercased().starts(with: query?.lowercased() ?? "") {
+                filteredData.append(string)
+            }
+        }
+        table.reloadData()
+        filtered = true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterText(searchText)
+    }
+    
+    
+    func setupData() {
+        data.append("John")
+        data.append("Abe")
+        data.append("Idan")
+        data.append("Dan")
+        data.append("Zach")
+        data.append("Luke")
+        data.append("Kevin")
+        data.append("Briana")
+        data.append("Melenie")
+        data.append("Sarah")
+        data.append("Shawn")
+        data.append("Teri")
+        data.append("Tom")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filtered {
+            return filteredData.count
+        } else {
+            return data.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "YourCellIdentifier", for: indexPath)
+        
+        if filtered {
+            if indexPath.row < filteredData.count {
+                cell.textLabel?.text = filteredData[indexPath.row]
+            }
+        } else {
+            if indexPath.row < data.count {
+                cell.textLabel?.text = data[indexPath.row]
+            }
+        }
+        
+        return cell
+    }
 }
